3c66281c3e8608e147791973032c7afdd724f08b
Rob <github email address>
2018-01-19 07:44:15 -0500

    reformatted

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :category
   has_many :line_items
 
-   def self.available_items
+  def self.available_items
     if self.inventory >= 1
       # binding.pry
       self.title
