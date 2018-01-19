e90ff848ef8fc388eeaa2fa64dda89f1a3843b38
Rob <github email address>
2018-01-19 07:43:02 -0500

    added method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ActiveRecord::Base
   belongs_to :category
   has_many :line_items
-  
+
+   def self.available_items
+    if self.inventory >= 1
+      # binding.pry
+      self.title
+    end
+  end
+
 end
