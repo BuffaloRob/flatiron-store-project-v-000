cd09534f8b19fae870ce0f8bc65f25e145108840
Rob <github email address>
2018-01-21 10:53:43 -0500

    removed uneccessary code

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,11 +4,11 @@ class CartsController < ApplicationController
     @cart = Cart.find(params[:id])
   end
 
-  def create
-    @cart = Cart.find(session[:cart_id])
-  rescue ActiveRecord::RecordNotFound
-    @cart = Cart.create
-    session[:cart_id] = @cart.id
-  end
+  # def create
+  #   @cart = Cart.find(session[:cart_id])
+  # rescue ActiveRecord::RecordNotFound
+  #   @cart = Cart.create
+  #   session[:cart_id] = @cart.id
+  # end
   
 end
