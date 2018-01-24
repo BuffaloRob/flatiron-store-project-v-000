17c0ec523d8ba4599c03a51d5a4ce38846c218a8
BuffaloRob <peckham.rob@gmail.com>
2018-01-24 10:21:07 -0500

    second solution

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,33 +3,31 @@ class Cart < ActiveRecord::Base
   has_many :items, through: :line_items
   belongs_to :user
 
-  def total
-    items.map do |item| 
-      item.price
-    end.sum
+  def add_item(item_id)
+    if item_ids.include?(item_id.to_i)
+      current_line_item = line_items.find_by(item_id: item_id)
+      current_line_item.quantity += 1
+      current_line_item
+    else
+      line_items.build(item_id: item_id)
+    end
   end
 
- def add_item(item_id)
-    line_item = self.line_items.find_by(item_id: item_id)
-    if line_item
-      line_item.quantity += 1
-    else
-      line_item=self.line_items.build(item_id: item_id)
-    end
-    line_item
+  def total
+    line_items.inject(0) { |sum, line_item| sum + line_item.total}
   end
 
   def checkout
-    self.status = "submitted"
-    change_inventory
+    remove_inventory
+    user.remove_cart
+    update(status: 'submitted')
   end
 
-  def change_inventory
-    if self.status = "submitted"
-      self.line_items.each do |line_item|
-        line_item.item.inventory -= line_item.quantity
-        line_item.item.save
-      end
+  private
+
+  def remove_inventory
+    line_items.each do |line_item|
+      line_item.item.remove(line_item.quantity)
     end
   end
 
